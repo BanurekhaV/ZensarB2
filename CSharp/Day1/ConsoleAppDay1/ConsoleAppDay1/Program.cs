d065b785b29e70923ad55cca7e8d6389e4da536b
BanurekhaV <rekhasairam@hotmail.com>
2022-04-18 16:21:04 +0530

    Day1Csharp Program

---
+++ CSharp/Day1/ConsoleAppDay1/ConsoleAppDay1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ConsoleAppDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello All ! Welcome to DotNet Programming");
+             Program p = new Program(); //instantiating an object of class Program
+             int res = p.add(5, 6); // calling function
+             Console.WriteLine(res);
+             Console.WriteLine("Enter Your Name :");
+             string name = Console.ReadLine();
+             Console.WriteLine("Hello {0}, Welcome to CSharp",name); //place holders
+             Console.WriteLine("Hello" + " "+ name +" "+ "Welcome to CSharp"); //concatenation
+             Console.WriteLine($"Hello {name} welcome to CSharp"); //string interpolation*/
+            Student s = new Student();
+            s.getDetails();
+            s.DisplayDetails();
+            Console.Read();
+        }
+
+        public int add(int a, int b) // called function
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CSharp/Day1/ConsoleAppDay1/ConsoleAppDay1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay1
+{
+    class Student
+    {
+        //Fields Declaration
+        int RollNo;
+        string Name;
+        float Marks;
+
+        //Function declaration and definition
+        public void getDetails()
+        {
+            Console.WriteLine("Enter Roll No :");
+            RollNo =int.Parse(Console.ReadLine()); // one way to convert string into int
+            //  RollNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter Marks :");
+            Marks = float.Parse(Console.ReadLine());
+            //Marks=Convert.ToSingle(Console.ReadLIne());
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Student Named {Name} with RollNo {RollNo} has secured {Marks} marks");
+        }
+    }
+}
