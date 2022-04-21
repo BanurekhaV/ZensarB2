393c6a354563025781827c2cd6649ddafcdf5d45
BanurekhaV <rekhasairam@hotmail.com>
2022-04-21 13:28:08 +0530

    arrays

---
+++ CSharp/Day3/ConsoleAppDay3/ConsoleAppDay3/ArraysEg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay3
+{
+    class ArraysEg
+    {
+        //examples for 2 dimensional arrays
+        public void TwoDimension()
+        {
+            //declare a 2 dimensional array
+            int[,] myarr = new int[2, 3];  //2 rows and 3 columns
+            //declare and to initialize the 2 dimensional array
+            int[,] myarr1 = new int[2, 3] { {1,2,3 }, {4,5,6 } };
+
+            Console.WriteLine(myarr1[0, 1]); // returns a value of 2 as per the above values
+
+            //to change the value of a 2 dimensional array
+            myarr1[1, 0] = 14;
+
+            Console.WriteLine(myarr1[1, 0]);
+        }
+    }
+}
--- CSharp/Day3/ConsoleAppDay3/ConsoleAppDay3/Program.cs
+++ CSharp/Day3/ConsoleAppDay3/ConsoleAppDay3/Program.cs
@@ -46,12 +46,33 @@ namespace ConsoleAppDay3
                 Console.WriteLine("Enter nos into array of 5");
                 myarray[x] = Convert.ToInt32(Console.ReadLine()); //x+5
             }
-
-            for(int x=0; x<5;x++)
+            //for loop to print all the values in the myarray
+            for(int x=0; x<myarray.Length;x++)
             {
                 Console.WriteLine(myarray[x]);
             }
-            Console.Read();
+
+            //foreach loop to print all the values in the myarray
+
+            Console.WriteLine("======Using Foreach Loop=====");
+            foreach(int i in myarray)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("old value at 3rd place is :"+ " "+ myarray[2]);
+            //change the value at a particular index
+            myarray[2] = 100;
+            Console.WriteLine("New value at 3rd place is :" + " " + myarray[2]);
+            Console.WriteLine("The length is :" + " " + myarray.Length);
+            Console.WriteLine("The smallest element is :" + " " + myarray.Min());
+            Console.WriteLine("The largest element is :" + " "+ myarray.Max());
+            Console.WriteLine("The sum of all elements is :"+ " " + myarray.Sum());
+
+            Console.WriteLine("-----------2 dimensional array values------");
+            ArraysEg ae = new ArraysEg();
+            ae.TwoDimension();
+              Console.Read();
         }
         
     }
