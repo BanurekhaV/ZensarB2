e921da2f819bf04fb52483e52f33371da59c6d76
BanurekhaV <rekhasairam@hotmail.com>
2022-04-25 17:55:00 +0530

    Day6 part 2

---
+++ CSharp/Day6/ConsoleAppDay6/ConsoleAppDay6/HidingEg.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace ConsoleAppDay6
+{
+    class A1
+    {
+        public int i = 0;
+
+        public void Method1(string msg)
+        {
+            Console.WriteLine("Message from Base class {0}", msg);
+        }
+    }
+
+    class B1: A1
+    {
+        new int i;  // this  i hides the i in A1 class - name hiding
+
+        public B1(int a, int b)
+        {
+            base.i = a;  // i in A1 class
+            i = b;  //i in B1 class           
+        }
+
+        public new void Method1(string s) //method hiding
+        {
+            Console.WriteLine("Message from Derived Class {0}", s);
+            base.Method1("hello");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("i in Base calss A1 :" + " " +base.i);
+            Console.WriteLine("i in Derived class B1 :" + " "+i);
+        }
+    }
+    class HidingEg
+    {
+        static void Main()
+        {
+            B1 b1obj = new B1(2, 3);
+            b1obj.Show();
+
+            SqlConnection con = new SqlConnection();
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day6/ConsoleAppDay6/ConsoleAppDay6/OverridingEg.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay6
+{
+    class Shape   //base class
+    {
+        protected float R, L, B;
+
+        //this function is calculating the area of circle. It is marked virtual to allow
+        //overriding in the derived class
+        public virtual float Area()            
+        {
+            return 3.14f; ;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public void GetLB()
+        {
+            Console.WriteLine("Enter Length :");
+            L = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Breadth :");
+            B = Convert.ToSingle(Console.ReadLine());
+           // float temp;
+            // bool s = float.TryParse(Console.ReadLine(), out temp);
+           // B = temp;
+        }
+        public override float Area()
+        {
+            return L * B;
+        }
+    }
+
+    class Circle : Shape
+    {
+        public void GetRadius()
+        {
+            Console.WriteLine("Enter Radius :");
+            R = float.Parse(Console.ReadLine());
+        }
+        public override float Area()
+        {
+            Console.WriteLine(base.Area()); 
+            return 3.14f * R * R;
+           // return base.Area();
+        }
+    }
+    class OverridingEg
+    {
+        static void Main()
+        {
+            Rectangle r = new Rectangle();
+            r.GetLB();
+            Console.WriteLine("Area of Rectangle is {0}", r.Area());
+            Circle c = new Circle();
+            c.GetRadius();
+            Console.WriteLine("Area of Circle is {0}", c.Area());
+            Console.Read();
+        }
+    }
+}
