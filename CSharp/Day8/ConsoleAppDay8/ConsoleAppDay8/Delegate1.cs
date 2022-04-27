08df001f56f8f5bba787ff04020d418893809da6
BanurekhaV <rekhasairam@hotmail.com>
2022-04-27 21:55:13 +0530

    day8

---
+++ CSharp/Day8/ConsoleAppDay8/ConsoleAppDay8/Delegate1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay8
+{
+    public delegate void EmployeeDelegate(string s); //declaration of a delegate 
+    class Delegate1
+    {
+        public void accept(string s)
+        {
+            Console.WriteLine("Hello " +  " " + s);
+        }
+        public static void Show(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public string Message()
+        {
+            return "Hi";
+        }
+
+        static void Main()
+        {
+            Delegate1 del1 = new Delegate1();
+            EmployeeDelegate empd1 = new EmployeeDelegate(Delegate1.Show);
+            EmployeeDelegate empd2 = new EmployeeDelegate(del1.accept);
+
+            empd1("Banurekha"); //calling a delegate that internally calls the function that it is pointing to
+            empd2.Invoke("Venkat");
+            Console.Read();
+        }
+    }
+}
