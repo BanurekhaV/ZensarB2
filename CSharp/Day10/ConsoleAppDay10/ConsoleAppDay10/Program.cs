0c3b02a77aef050240024980593360457dd94ff6
BanurekhaV <rekhasairam@hotmail.com>
2022-04-29 11:17:04 +0530

    files

---
+++ CSharp/Day10/ConsoleAppDay10/ConsoleAppDay10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleAppDay10
+{
+    class Program
+    {
+        //using Binary Reader and Writer classes
+
+        static void WriteBinaryFile()
+        {
+            using(BinaryWriter writer= new BinaryWriter(File.Open(@"E:\Banu\Zensar\zensarbinary.bin",FileMode.Create)))
+            {
+                writer.Write("0x8023440");
+                writer.Write("This is Binary Writer");
+                writer.Write(true);
+            }
+        }
+
+        static void ReadBinaryFile()
+        {
+            BinaryReader reader = new BinaryReader(File.Open("E:\\Banu\\Zensar\\zensarbinary.bin", FileMode.Open));
+            Console.WriteLine("Data 1 :" + " " + reader.ReadString());
+            Console.WriteLine("Data 2 :" + " " + reader.ReadString());
+            Console.WriteLine("Data3 :" + " " + reader.ReadBoolean());
+        }
+        static void Main(string[] args)
+        {
+            WriteBinaryFile();
+            ReadBinaryFile();
+            Console.Read();
+        }
+    }
+}
--- CSharp/Day9/ConsoleAppDay9/ConsoleAppDay9/Deep_Shallow_Copy.cs
+++ CSharp/Day9/ConsoleAppDay9/ConsoleAppDay9/Deep_Shallow_Copy.cs
@@ -21,6 +21,12 @@ namespace ConsoleAppDay9
         {
             return this.MemberwiseClone();
         }
+
+        public Person DeepCopy()
+        {
+            Person dcopy = new Person(this.Age, desc.Fname, desc.Lname);
+            return dcopy;
+        }
     }
 
     class PersonDescription
@@ -40,11 +46,26 @@ namespace ConsoleAppDay9
         {
             Person p1 = new Person(40, "Steve", "Jobbs");
             Person p2 = (Person)p1.ShallowCopy();
-            Console.WriteLine(p1.Age + " " + p1.desc.Fname);
+            Console.WriteLine(p1.Age + " " + p1.desc.Fname+" "+p2.desc.Fname);
             p2.desc.Fname = "Henry";
-            Console.WriteLine(p1.desc.Fname);
-            Console.WriteLine(p2.desc.Fname);
+            Console.WriteLine($"p1s fname is {p1.desc.Fname}");
+            Console.WriteLine($"p2s fname is {p2.desc.Fname}");
 
+            Person p3 = p1.DeepCopy();
+            Console.WriteLine($"p3 data  is :{p3.Age}, {p3.desc.Lname},{p3.desc.Fname}");
+            Console.WriteLine($"p1 data is : {p1.Age}, {p1.desc.Lname},{p1.desc.Fname}");
+            p3.desc.Fname = "Robert";
+            Console.WriteLine("--------------");
+            Console.WriteLine($"p3 data  is :{p3.Age}, {p3.desc.Lname},{p3.desc.Fname}");
+            Console.WriteLine($"p1 data is : {p1.Age}, {p1.desc.Lname},{p1.desc.Fname}");
+            Console.WriteLine("-------------");
+            Console.WriteLine(p1.GetHashCode());
+            Console.WriteLine(p3.GetHashCode());
+
+            //if we equate 2 objects then
+            p2 = p1;
+            Console.WriteLine(p1.GetHashCode());
+            Console.WriteLine(p2.GetHashCode());
             Console.Read();
         }
     }
