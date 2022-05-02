7638fe613ceee1fadc3c12e9ce826a3e188a6818
BanurekhaV <rekhasairam@hotmail.com>
2022-05-02 11:04:07 +0530

    Disposable+using

---
+++ CSharp/Day11/ConsoleAppDay11/ConsoleAppDay11/DisposableEg.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay11
+{
+    class DisposableEg : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Releasing all Resources..");
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Using Various Resources..");
+        }
+
+        public DisposableEg()
+        {
+            Console.WriteLine("Allocating various Resources...");
+        }
+        static void Main()
+        {
+            //eg 1.
+            //string multilines = @"This is line no 1.
+            //This is line 2 
+            //Here is line 3
+            // There goes line 4
+            //  This is the final and fifth line";
+
+            //using (var reader = new StringReader(multilines))
+            //{
+            //    string item;
+            //    do
+            //    {
+            //        item = reader.ReadLine();
+            //        Console.WriteLine(item);
+            //    } while (item != null);
+            //}
+
+            ////eg 2
+            //using(var sreader= new StreamReader("E:\\Banu\\Zensar\\Batch 2\\firstgitdata.txt") )
+            //{
+            //    Console.WriteLine(sreader.ReadToEnd());
+            //}
+
+            ////eg 3 using try finally which is equivalent to using block with some coding
+            //var streader = new StreamReader(@"E:\Banu\Zensar\Batch 2\firstgitdata.txt");
+            //try
+            //{
+            //    Console.WriteLine(streader.ReadToEnd());
+            //}
+            //finally
+            //{
+            //    if (streader != null)
+            //        streader.Dispose();
+            //}
+
+            //eg 4 using IDisposable interface
+            using(var dispeg =  new DisposableEg())
+            {
+                dispeg.DoSomething();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day11/ConsoleAppDay11/ConsoleAppDay11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread t1 = new Thread(DisplayNumbers); // it invokes the delegate threadstart
+            //t1.Start();
+
+            //the above can also be done thru a threadstart object itself
+            ThreadStart obj = new ThreadStart(DisplayNumbers);
+            // Thread t1 = new Thread(obj);
+            // t1.Start();
+            obj();
+            Console.Read();
+        }
+
+        static void DisplayNumbers()
+        {
+            for(int i=1;i<=5;i++)
+            {
+                Console.WriteLine("Method " + i);
+            }
+        }
+    }
+}
