7ba86e16c1cb82619eeb1916a250af0bc905076b
BanurekhaV <rekhasairam@hotmail.com>
2022-05-04 11:05:11 +0530

    delegate types part1

---
+++ CSharp/Day12/ConsoleAppDay12/ConsoleAppDay12/DelegateTypes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay12
+{
+    class DelegateTypes
+    {        
+        static void Main()
+        {
+            //1. func delegate with 0 parameters- anonymous
+            //Func<int> getrandomnumber = delegate ()
+            //{
+            //    Random rnd = new Random();
+            //    return rnd.Next(1, 100);
+            //};
+            //int randomnumber = getrandomnumber();
+            //Console.WriteLine(randomnumber);
+
+            //1. with lambda
+            Func<int> getrandomnumber2 = () => new Random().Next(1, 100);
+            int randomnumber = getrandomnumber2();
+            Console.WriteLine(randomnumber);
+
+            //one more example of func delegate with lambda
+            //we need to add 2 numbers and return the sum
+            Func<int, int, int> sum = (x, y) => x + y;
+            int total = sum(20, 20);
+            Console.WriteLine(total);
+
+             //func delegate with 1 input
+            Func<string, bool> strdel = String.IsNullOrEmpty;
+            bool result = strdel("CSharp Delegates"); //making a call thru delegate object to a IsNullOrEmpty function
+            //pointed by the delegae
+            Console.WriteLine($"String.IsNullOrEmpty returned : {result}");
+
+            //func delegate with 2 inputs
+            Func<double, double, double> powerdel = Math.Pow;
+            double power = powerdel(2, 3); //call the delegate pointing function
+            Console.WriteLine($"2 ^ 3 is : {power}");
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day12/ConsoleAppDay12/ConsoleAppDay12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay12
+{
+    delegate void Print(int val);
+    delegate void ArithmeticHandler(int fnumber, int snumber);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Print p = delegate (int x) { Console.WriteLine("Inside Anonymous Function:{0}", x); };
+            p(100);
+
+            //anonymous method can access the variable defined in an outer function
+            Print p1 = delegate (int x) { x += i; Console.WriteLine("Anonymous method accessing outside variable {0}", x); };
+            p1(50);
+
+            //using method expression to call anonymous 
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] evennos = Array.FindAll(arr, delegate (int num) { return num % 2 == 0; });
+            Console.WriteLine("The even nos are :");
+
+            foreach(int z in evennos)
+            {
+                Console.Write(z + " ");
+            }
+
+            //anonymous methods with lambda expressions
+            ArithmeticHandler ah = delegate (int fnum, int snum)
+            {
+                Console.WriteLine("Addition of {0} and {1} is {2}", fnum,snum,fnum+snum);
+            };
+            ah(5, 10);
+
+            // with lambda
+            ArithmeticHandler ah2 = (fnum, snum) =>
+            {
+                Console.WriteLine("Addition of {0} and {1} is {2} using lambda", fnum, snum, fnum + snum);
+            };
+            ah2(5, 10);
+            Console.Read();
+        }
+    }
+}
