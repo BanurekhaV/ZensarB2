d92c7e0fef9b0d2c138f96e94dc9ccbea129aac1
BanurekhaV <rekhasairam@hotmail.com>
2022-05-06 11:07:07 +0530

    tasks

---
+++ CSharp/Day14/ConsoleAppDay14/ConsoleAppDay14/AsyncAwaitEg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleAppDay14
+{
+    class AsyncAwaitEg
+    {
+        public static void Main()
+        {
+            FileRead fr = new FileRead();
+            Console.WriteLine("Fileread object created..");
+            fr.filereadoperation();
+            Console.Read();
+        }
+    }
+
+    class FileRead
+    {
+        public async void filereadoperation()
+        {
+            try
+            {
+                StreamReader sr = File.OpenText(@"E:\Banu\Zensar\Batch 2\firstgitdata.txt");
+                Console.WriteLine($"The first line of the file is : {sr.ReadLine()}");
+                sr.Close();
+            }
+            catch { await FileNotFound(); }
+            finally { await ExitProgram(); }
+        }
+        async Task FileNotFound()
+        {
+            Console.WriteLine("Specified File not found.. Please check the name and location");
+        }
+        async Task ExitProgram()
+        {
+            Console.WriteLine("\n Press any key to exit");
+        }
+    }
+}
---
+++ CSharp/Day14/WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClickMe = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ClickMe
+            // 
+            this.ClickMe.Location = new System.Drawing.Point(191, 254);
+            this.ClickMe.Name = "ClickMe";
+            this.ClickMe.Size = new System.Drawing.Size(75, 23);
+            this.ClickMe.TabIndex = 0;
+            this.ClickMe.Text = "Click Me";
+            this.ClickMe.UseVisualStyleBackColor = true;
+            this.ClickMe.Click += new System.EventHandler(this.ClickMe_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ClickMe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClickMe;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
