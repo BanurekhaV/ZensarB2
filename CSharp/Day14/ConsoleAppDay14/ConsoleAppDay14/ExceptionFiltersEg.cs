c118754c7564c8a2bdeb6c085f3f5fc4731b94a7
BanurekhaV <rekhasairam@hotmail.com>
2022-05-06 17:11:55 +0530

    enhancements 2

---
+++ CSharp/Day14/ConsoleAppDay14/ConsoleAppDay14/ExceptionFiltersEg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay14
+{
+    class ExceptionFiltersEg
+    {
+        static void Main()
+        {
+            try
+            {
+                int a = 10, b = 0, c;
+                c = a / b;
+                int[] arr = new int[5];
+                arr[6] = 10;                
+            }
+            catch(Exception e) when (e.GetType().ToString() == "System.IndexOutOfRangeException")
+            {
+                //do something here
+                SomeJob();
+                Console.Read();
+            }
+            catch(Exception e) when (e.GetType().ToString() == "System.DivideByZeroException")
+            {
+                Console.WriteLine("Encounterd a divide by zero exception.. try again");
+            }
+
+        }
+        static void SomeJob()
+        {
+            Console.WriteLine("This is a new Job being performed....");
+        }
+    }
+}
---
+++ CSharp/Day14/ConsoleAppDay14/ConsoleAppDay14/NameofEg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay14
+{
+    class NameofEg
+    {
+        int[] arr = new int[5];
+
+        #region
+        public static void Main()
+        {
+            NameofEg noe = new NameofEg();
+            try
+            {
+                noe.Display(noe.arr);
+            }
+            catch(IndexOutOfRangeException ie)
+            {
+                //we can display the name of the method that throws an exception
+                // Console.WriteLine(ie.Message); usual way oh handling error
+                ///summary
+                ///the name of operator is used here to check which function is throwing an exception
+                Console.WriteLine(nameof(noe.arr));
+                Console.WriteLine(nameof(NameofEg));
+                Console.WriteLine($"The Method :{nameof(noe.Display)} - resulted in an {ie.Message}");
+            }
+            Console.Read();
+        }
+
+       public int Display(int[] a)
+        {
+            a[6] = 10;
+            return a[6];
+        }
+        #endregion
+    }
+
+}
