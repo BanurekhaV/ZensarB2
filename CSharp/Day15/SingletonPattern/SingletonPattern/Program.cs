18402f10bb58267f1bde3a325fd1eb8cf3d874df
BanurekhaV <rekhasairam@hotmail.com>
2022-05-09 14:11:01 +0530

    Singleton

---
+++ CSharp/Day15/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SingleTonObject
+    {
+        //create static object of the class and creating it through a private constructor
+        private static SingleTonObject sobj = new SingleTonObject();
+
+        //private constructor
+        private SingleTonObject() { }
+
+        //Method to retrieve the one and only object of the class
+        public static SingleTonObject getObject()
+        {
+            return sobj;
+        }
+
+        public void DisplayObject()
+        {
+            Console.WriteLine("Hi.. This is a SingleTon Object");
+        }
+    }
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Geting a singleton class object hrough the method of the class and not the constructor
+            SingleTonObject s = SingleTonObject.getObject();
+                s.DisplayObject();
+            SingletonEg2 s2 = SingletonEg2.GetInstance;
+            s2.ShowDetails("Client 1");
+            SingletonEg2 s3 = SingletonEg2.GetInstance;
+            s3.ShowDetails("Client 2");
+                Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day15/SingletonPattern/SingletonPattern/SingletonEg2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class SingletonEg2
+    {
+        private static int counter = 0;
+        private static SingletonEg2 _single = null;
+
+        //returning the only one object thru a property
+        public static SingletonEg2 GetInstance
+        {
+            get
+            {
+                if (_single == null)
+                    _single = new SingletonEg2();
+                return _single;
+            }
+        }
+
+        private SingletonEg2()
+        {
+            counter++;
+            Console.WriteLine("Counter Value is :" + " " + counter.ToString());
+        }
+
+        public void ShowDetails(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
