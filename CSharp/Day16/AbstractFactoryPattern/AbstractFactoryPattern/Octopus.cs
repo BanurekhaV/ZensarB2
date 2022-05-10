a035388bd927e167ab15d1cc84e5efefa80b664a
BanurekhaV <rekhasairam@hotmail.com>
2022-05-10 10:47:30 +0530

    Absract Factory Pattern

---
+++ CSharp/Day16/AbstractFactoryPattern/AbstractFactoryPattern/Octopus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    public class Octopus : Animal
+    {
+        public string speak()
+        {
+            return "SQUAWCK";
+        }
+    }
+}
