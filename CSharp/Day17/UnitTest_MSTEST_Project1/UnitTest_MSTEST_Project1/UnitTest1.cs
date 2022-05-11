30af764b8bb83a95040741ea3aa17862ec195ee5
BanurekhaV <rekhasairam@hotmail.com>
2022-05-11 17:23:06 +0530

    mstest

---
+++ CSharp/Day17/UnitTest_MSTEST_Project1/UnitTest_MSTEST_Project1/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest_MSTEST_Project1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [AssemblyInitialize]
+
+        public static void beforeAssembly(TestContext tc)
+        {
+            Console.WriteLine("Before Assembly");
+        }
+        [ClassInitialize]
+
+        public static void Beforeclass(TestContext tc)
+        {
+            Console.WriteLine("This is before the class");
+        }
+
+        
+        [TestInitialize]
+
+        public void BeforeAllTestMethods()
+        {
+            Console.WriteLine("Before every Test Method");
+        }
+
+        [TestCleanup]
+
+        public void AfterAllTestMethods()
+        {
+            Console.WriteLine("After every test method");
+        }
+
+        [ClassCleanup]
+
+        public static void AfterClass()
+        {
+            Console.WriteLine("After the class");
+        }
+
+        [AssemblyCleanup]
+
+        public static void afterAssembly()
+        {
+            Console.WriteLine("After Assembly");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This is Test Method 1");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("This is Method 2");
+        }
+
+        [TestMethod]
+
+        public void TestMethod3()
+        {
+            Console.WriteLine("This is Method 3");
+        }
+    }
+}
---
+++ CSharp/Day17/UnitTest_MSTEST_Project1/UnitTest_MSTEST_Project1/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_MSTEST_Project1
+{
+    [TestClass]
+    class UnitTest2
+    {
+        [TestMethod]
+        public void class2Method1()
+        {
+            Console.WriteLine("Method 1 of class 2");
+        }
+    }
+}
