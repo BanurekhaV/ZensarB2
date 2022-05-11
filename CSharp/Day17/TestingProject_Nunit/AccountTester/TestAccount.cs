af405f6e557625ddea880d7bcae6e0a32dbb2283
BanurekhaV <rekhasairam@hotmail.com>
2022-05-11 10:48:02 +0530

    nunit example1

---
+++ CSharp/Day17/TestingProject_Nunit/AccountTester/TestAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestingProject_Nunit;
+
+namespace AccountTester
+{
+    [TestFixture]
+    public class TestAccount
+    {
+        
+        Accounts acc;
+
+        [SetUp]
+        public void SetupObjects()
+        {
+            //arrange
+            acc = new Accounts("12345");
+        }
+
+        [Test]
+        public void TestingDepositMethod()
+        {
+            //act
+            acc.Deposit(1000);
+
+            //assert
+            Assert.AreEqual(1000, acc.CheckBalance());
+        }
+
+        [Test]
+        public void TestingDeposit_ifbalance_is_changed_should_fail()
+        {
+            acc.accountbalance = 2000;
+            acc.Deposit(1000);
+            Assert.LessOrEqual(acc.CheckBalance(),4000);
+        }
+        public void TestingWithdrawMethod()
+        {
+            acc.Withdraw(100);
+        }
+
+        [Test]
+        public void Withdraw_should_throw_exception()
+        {
+            Assert.Throws<Exception>(TestingWithdrawMethod);
+        }
+    }
+}
---
+++ CSharp/Day17/TestingProject_Nunit/TestingProject_Nunit/Accounts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingProject_Nunit
+{
+    public class Accounts
+    {
+        private string accno;
+        private float balance;
+
+        public float accountbalance
+        {
+            set { balance = value; }
+        }
+        public Accounts(string accno)
+        {
+            this.accno = accno;
+        }
+
+        public float CheckBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(float amt)
+        {
+            balance += amt;
+        }
+
+        public void Withdraw(float amt)
+        {
+            if (balance >= amt)
+                balance -= amt;
+            else
+                throw new Exception("Not Enough Funds");
+        }
+    }
+}
