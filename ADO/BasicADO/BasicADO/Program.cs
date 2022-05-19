6d574b2a3e36c544eb3b3a66b3163fea7dd4a4dd
BanurekhaV <rekhasairam@hotmail.com>
2022-05-19 18:05:33 +0530

    ado day1

--- ADO/BasicADO/BasicADO/Program.cs
+++ ADO/BasicADO/BasicADO/Program.cs
@@ -15,12 +15,50 @@ namespace BasicADO
         public static SqlDataReader dr;
         static void Main(string[] args)
         {
-           //
-           //SelectData();
-            GetScalarFunc();
+            // InsertProductData();
+            // SelectEmployeeData();
+            //  GetScalarFunc();
+            DeleteProduct();
             Console.Read();
         }
 
+        public static void DeleteProduct()
+        {
+            con = getCon();
+            Console.WriteLine("Enter Product id to delete :");
+            int pid = Convert.ToInt32(Console.ReadLine());
+            cmd = new SqlCommand("delete from product where productid=@pid", con);
+            cmd.Parameters.AddWithValue("@pid", pid);
+            int res=cmd.ExecuteNonQuery(); 
+            if(res>0)
+                Console.WriteLine("record deleted");
+            else
+                Console.WriteLine("Not deleted");
+        }
+        public static void InsertProductData()
+        {
+            con = getCon();
+            Console.WriteLine("Enter ProductId:");
+            int pid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Description :");
+            string pdesc = Console.ReadLine();
+            Console.WriteLine("Enter Product Price :");
+            int pprice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Available Qty :");
+            int pqty = Convert.ToInt32(Console.ReadLine());
+            cmd = new SqlCommand("Insert into Product values(@id,@desc,@price,@qty)", con);
+            cmd.Parameters.AddWithValue("@id", pid);
+            cmd.Parameters.AddWithValue("@desc", pdesc);
+            cmd.Parameters.AddWithValue("@price", pprice);
+            cmd.Parameters.AddWithValue("@qty", pqty);
+            int result=cmd.ExecuteNonQuery();
+            if(result>0)
+            {
+                Console.WriteLine("Added one record.");
+            }
+            else
+                Console.WriteLine("Something went wrong.. did not insert");
+        }
         public static void GetScalarFunc()
         {
             con = getCon();
@@ -28,7 +66,7 @@ namespace BasicADO
             int empcount = Convert.ToInt32(cmd.ExecuteScalar()); // execute scalar returns object
             Console.WriteLine("No.of Employees :{0}",empcount);
         }
-        public static void SelectData()
+        public static void SelectEmployeeData()
         {
             con = getCon();
             // cmd = new SqlCommand("select * from tblemployee", con);
