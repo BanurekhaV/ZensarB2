63646a238f59ba66a3c8f0f1f022585652ac477d
BanurekhaV <rekhasairam@hotmail.com>
2022-05-25 18:15:54 +0530

    sqlmetal+html

---
+++ EF/Linq_to_Sql_Sqlmetal/Linq_to_Sql_Sqlmetal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linq_to_Sql_Sqlmetal.DAL;
+
+namespace Linq_to_Sql_Sqlmetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindContext dbc = new NorthwindContext();
+            var regiondata = dbc.Region.ToList();
+
+            var expprd = dbc.Ten_Most_Expensive_Products();
+
+            foreach (var r in regiondata)
+            {
+                Console.WriteLine($"{r.RegionID}, {r.RegionDescription}");
+            }
+            Console.WriteLine("-------------------------");
+
+            foreach (var v in expprd)
+            {
+                Console.WriteLine(v.TenMostExpensiveProducts + "  " + v.UnitPrice);
+            }
+            Console.Read();
+
+        }
+    }
+}
