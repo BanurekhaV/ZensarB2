218fb2f885fb8395915dd4dce99dc2a4227d5d11
BanurekhaV <rekhasairam@hotmail.com>
2022-06-07 17:06:30 +0530

    day2 ef

---
+++ MVC/Day2EF_Prj/Day2EF_Prj/Controllers/MultipleTableController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day2EF_Prj.Models;
+
+namespace Day2EF_Prj.Controllers
+{
+    public class MultipleTableController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: MultipleTable
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //to view data from multiple tables using navigation properties
+        public ActionResult Cust_order_Details()
+        {
+            return View(db.Orders.ToList());
+        }
+    }
+}
